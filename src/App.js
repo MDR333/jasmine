cccf22b1151be32a2f3c156251e5a06db73aff96
Mack <mackramirez2000@gmail.com>
2023-02-03 22:52:57 -0600

    added graphql and storage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
-import { API } from "aws-amplify";
+import { API, Storage } from 'aws-amplify';
 import {
   Button,
   Flex,
   Heading,
+  Image,
   Text,
   TextField,
   View,
   withAuthenticator,
-} from "@aws-amplify/ui-react";
+} from '@aws-amplify/ui-react';
 import { listNotes } from "./graphql/queries";
 import {
   createNote as createNoteMutation,
@@ -27,16 +28,28 @@ const App = ({ signOut }) => {
   async function fetchNotes() {
     const apiData = await API.graphql({ query: listNotes });
     const notesFromAPI = apiData.data.listNotes.items;
+    await Promise.all(
+      notesFromAPI.map(async (note) => {
+        if (note.image) {
+          const url = await Storage.get(note.name);
+          note.image = url;
+        }
+        return note;
+      })
+    );
     setNotes(notesFromAPI);
   }
 
   async function createNote(event) {
     event.preventDefault();
     const form = new FormData(event.target);
+    const image = form.get("image");
     const data = {
       name: form.get("name"),
       description: form.get("description"),
+      image: image.name,
     };
+    if (!!data.image) await Storage.put(data.name, image);
     await API.graphql({
       query: createNoteMutation,
       variables: { input: data },
@@ -45,9 +58,10 @@ const App = ({ signOut }) => {
     event.target.reset();
   }
 
-  async function deleteNote({ id }) {
+  async function deleteNote({ id, name }) {
     const newNotes = notes.filter((note) => note.id !== id);
     setNotes(newNotes);
+    await Storage.remove(name);
     await API.graphql({
       query: deleteNoteMutation,
       variables: { input: { id } },
@@ -55,52 +69,53 @@ const App = ({ signOut }) => {
   }
 
   return (
-    <View className="App">
-      <Heading level={1}>My Notes App</Heading>
-      <View as="form" margin="3rem 0" onSubmit={createNote}>
-        <Flex direction="row" justifyContent="center">
-          <TextField
+    <div className="App">
+      <h1>My Notes App</h1>
+      <form onSubmit={createNote}>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <input
+            type="text"
             name="name"
             placeholder="Note Name"
-            label="Note Name"
-            labelHidden
-            variation="quiet"
             required
           />
-          <TextField
+          <input
+            type="text"
             name="description"
             placeholder="Note Description"
-            label="Note Description"
-            labelHidden
-            variation="quiet"
             required
           />
-          <Button type="submit" variation="primary">
-            Create Note
-          </Button>
-        </Flex>
-      </View>
-      <Heading level={2}>Current Notes</Heading>
-      <View margin="3rem 0">
+          <input type="file" name="image" />
+          <button type="submit">Create Note</button>
+        </div>
+      </form>
+      <h2>Current Notes</h2>
+      <div style={{ display: "flex", justifyContent: "center" }}>
         {notes.map((note) => (
-          <Flex
+          <div
             key={note.id || note.name}
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
           >
-            <Text as="strong" fontWeight={700}>
-              {note.name}
-            </Text>
-            <Text as="span">{note.description}</Text>
-            <Button variation="link" onClick={() => deleteNote(note)}>
-              Delete note
-            </Button>
-          </Flex>
+            <strong>{note.name}</strong>
+            <span>{note.description}</span>
+            {note.image && (
+              <img
+                src={note.image}
+                alt={`visual aid for ${notes.name}`}
+                style={{ width: 400 }}
+              />
+            )}
+            <button onClick={() => deleteNote(note)}>Delete note</button>
+          </div>
         ))}
-      </View>
-      <Button onClick={signOut}>Sign Out</Button>
-    </View>
+      </div>
+      <button onClick={signOut}>Sign Out</button>
+    </div>
   );
 };
 
